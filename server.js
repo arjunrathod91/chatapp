94dff6f9eb1dcc08a335bef08da473f238b7eb20
ArjunRathod-Remote_Connect <138087179+arjunrathod91@users.noreply.github.com>
2023-10-18 15:12:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const app = express()
 const http = require('http').createServer(app)
 
 const PORT = process.env.PORT || 3240
-const HOST = '127.0.0.1'
-http.listen(PORT,HOST,()=>{
+http.listen(PORT,()=>{
     console.log(`Listning on Port ${PORT}`)
 })
 
